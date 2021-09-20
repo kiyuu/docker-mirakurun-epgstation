25a94d96aaa5499a9f2d92ffe3dadedec7baba43
kiyu <xiukyxiglht@outlook.jp>
2021-09-20 21:16:04 +0900

    微修正

--- epgstation/config/update-thumbnail.sh
+++ epgstation/config/update-thumbnail.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+#
 curl -sX DELETE http://localhost:8888/api/thumbnails/$(curl -sX GET http://localhost:8888/api/recorded/$RECORDEDID?isHalfWidth=true | jq .thumbnails[0])
 curl -sX POST http://localhost:8888/api/thumbnails/videos/$VIDEOFILEID
