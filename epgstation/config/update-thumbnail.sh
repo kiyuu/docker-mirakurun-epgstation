f4e9e5e2f8e5185d86c01a94ff1e48afd3668314
kiyu <xiukyxiglht@outlook.jp>
2021-09-20 14:45:01 +0900

    サムネイル作成修正

--- epgstation/config/update-thumbnail.sh
+++ epgstation/config/update-thumbnail.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 
-if [ "$MODE" = "jlseNoVf" ]; then
-  curl -sX DELETE http://localhost:8888/api/thumbnails/$(curl -sX GET http://localhost:8888/api/recorded/$RECORDEDID?isHalfWidth=true | jq .thumbnails[0])
-  curl -sX POST http://localhost:8888/api/thumbnails/videos/$VIDEOFILEID
-fi
+curl -sX DELETE http://localhost:8888/api/thumbnails/$(curl -sX GET http://localhost:8888/api/recorded/$RECORDEDID?isHalfWidth=true | jq .thumbnails[0])
+curl -sX POST http://localhost:8888/api/thumbnails/videos/$VIDEOFILEID
